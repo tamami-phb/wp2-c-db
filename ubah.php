7820c2ff0da1ea735778d5b30826128b77190d8f
tamami <tamami.oka@gmail.com>
2022-03-28 15:26:52 +0700

    try another variation of pg_fetch

--- ubah.php
+++ ubah.php
@@ -3,8 +3,8 @@ require_once 'koneksi.php';
 
 $sql = "select * from mahasiswa where nim='" . $_GET['nim'] . "'";
 $result = pg_query($sql);
-$data = pg_fetch_object($result);
-echo $data;
+$data = pg_fetch_array($result);
+echo $data[0]['nim'];
 ?>
 
 <h1>Form Ubah Data</h1>
