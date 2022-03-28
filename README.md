f0afeb9eac2a680f405a14bdfbdf45fe13c2f64d
tamami <tamami.oka@gmail.com>
2022-03-28 15:21:45 +0700

    test the nim

---
+++ README.md
@@ -0,0 +1,3 @@
+# TM 6
+
+Menyelesaikan seluruh fitur CRUD dengan PHP dan melakukan _deploy_ ke Heroku.
---
+++ ubah.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'koneksi.php';
+
+$sql = "select * from mahasiswa where nim='" . $_GET['nim'] . "'";
+$result = pg_query($sql);
+$data = pg_fetch_object($result);
+?>
+
+<h1>Form Ubah Data</h1>
+
+<form method="POST">
+    NIM : 
+<?php
+echo '<input type="text" name="nim" required values="' . $data->nim . '"><br>';
+?>
+    NAMA : <input type="text" name="nama"><br>
+    KELAS : <input type="text" name="kelas"><br>
+    <input type="submit" value="Simpan">
+</form>
